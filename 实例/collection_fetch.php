80e9cbef7cbe62b9c19959b9d0b69c3db44bd975
luckyli <lizhihua@luckylideMacBook-Pro.local>
2014-12-09 09:55:31 +0800

    新建

---
+++ 实例/collection_fetch.php
@@ -0,0 +1,10 @@
+<?php 
+$stulist = array(
+	array("Code" => "10101", "Name"=> "周小敏", "Score"=> "500"),
+	array("Code" => "10102", "Name"=> "陆明明", "Score"=> "300"),
+	array("Code" => "10103", "Name"=> "陈天豪", "Score"=> "450"),
+	array("Code" => "10104", "Name"=> "刘真真", "Score"=> "600"),
+	array("Code" => "10105", "Name"=> "张明基", "Score"=> "720"),
+);
+echo json_encode($stulist);
+?>
